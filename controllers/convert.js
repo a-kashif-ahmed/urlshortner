b7f2e76fbaf86fb06e730dcabd5538b71b96e19c
a-kashif-ahmed <kashifmsd7@gmail.com>
2024-09-25 21:43:03 +0530

    mangoo

--- controllers/convert.js
+++ controllers/convert.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const shortid = require('shortid');
 const Url = require('../models/url');
 const { randomInt } = require('crypto');
-const udb = process.env.MONGODB ?? "mongodb+srv://dbadmin:kashif501@7@cluster0.asqex.mongodb.net/shorturl?retryWrites=true&w=majority"
-mongoose.connect(udb)
+const MANGODB_URI = process.env.MONGODB ?? "mongodb+srv://dbadmin:kashif501@7@cluster0.asqex.mongodb.net/shorturl?retryWrites=true&w=majority"
+mongoose.connect(MANGODB_URI);
 
 async function converts(body) {
     // const body = req.body;
